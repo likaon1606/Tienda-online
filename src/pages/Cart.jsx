33bdaa0d069ea60770bca97424ce08845c086af5
Ariel <ariel.fuentesgarcia@gmail.com>
2023-03-08 17:00:49 -0600

    cart incomplete

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,14 @@ import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import { Container, Row, Col } from "reactstrap";
 
+import tdImg from "../assets/images/arm-chair-01.jpg";
+import { motion } from "framer-motion";
+import { cartActions } from "../redux/slices/cartSlice";
+import { useSelector, useDispatch } from "react-redux";
+
 const Cart = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+
   return (
     <Helmet title='Cart'>
       <CommonSection title='Shopping Cart' />
@@ -12,17 +19,37 @@ const Cart = () => {
         <Container>
           <Row>
             <Col lg='9'>
-              <table className='table bordered'>
-                <thead>
-                  <tr>
-                    <th>Image</th>
-                    <th>Title</th>
-                    <th>Price</th>
-                    <th>Qty</th>
-                    <th>Delete</th>
-                  </tr>
-                </thead>
-              </table>
+              {cartItems.length === 0 ? (
+                <h2 className='fs-4 text-center'>No item added to the cart</h2>
+              ) : (
+                <table className='table bordered'>
+                  <thead>
+                    <tr>
+                      <th>Image</th>
+                      <th>Title</th>
+                      <th>Price</th>
+                      <th>Qty</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {cartItems.map((item, index) => (
+                      <tr key={index}>
+                        <td>
+                          <img src={item.imgUrl} alt='' />
+                        </td>
+                        <td>{item.productName}</td>
+                        <td>${item.price}</td>
+                        <td>{item.quantity}pcs</td>
+                        <td>
+                          <i class='ri-delete-bin-line'></i>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
             </Col>
 
             <Col lg='3'></Col>
