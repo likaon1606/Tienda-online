3d0f175814c761d93cdac8b58d159e73b0547499
Ariel <ariel.fuentesgarcia@gmail.com>
2023-03-07 20:46:24 -0600

    filter and search

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
+
+import "../styles/shop.css";
 
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
-}
+    <Helmet title='Shop'>
+      <CommonSection title='Products' />
 
-export default Shop
+      <section>
+        <Container>
+          <Row>
+            <Col lg='3' md='3'>
+              <div className='filter_widget'>
+                <select>
+                  <option>Filter By Category</option>
+                  <option value='sofa'>Sofa</option>
+                  <option value='mobile'>Mobile</option>
+                  <option value='chair'>Chair</option>
+                  <option value='watch'>Watch</option>
+                  <option value='wireless'>Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='3' md='3'>
+              <div className='filter_widget'>
+                <select>
+                  <option>Sort By</option>
+                  <option value='ascending'>Ascending</option>
+                  <option value='descending'>Descending</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='6' md='6'>
+              <div className='search_box'>
+                <input type='text' placeholder='Search...' />
+                <span>
+                  <i class='ri-search-line'></i>
+                </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Shop;
