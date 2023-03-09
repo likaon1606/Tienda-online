ec850261d061e8b66de43dd9675be5108b41246a
Ariel <ariel.fuentesgarcia@gmail.com>
2023-03-08 18:50:26 -0600

    cart complete

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,13 +4,15 @@ import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import { Container, Row, Col } from "reactstrap";
 
-import tdImg from "../assets/images/arm-chair-01.jpg";
 import { motion } from "framer-motion";
 import { cartActions } from "../redux/slices/cartSlice";
 import { useSelector, useDispatch } from "react-redux";
 
+import { Link } from "react-router-dom";
+
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
 
   return (
     <Helmet title='Cart'>
@@ -35,28 +37,62 @@ const Cart = () => {
 
                   <tbody>
                     {cartItems.map((item, index) => (
-                      <tr key={index}>
-                        <td>
-                          <img src={item.imgUrl} alt='' />
-                        </td>
-                        <td>{item.productName}</td>
-                        <td>${item.price}</td>
-                        <td>{item.quantity}pcs</td>
-                        <td>
-                          <i class='ri-delete-bin-line'></i>
-                        </td>
-                      </tr>
+                      <Tr item={item} key={index} />
                     ))}
                   </tbody>
                 </table>
               )}
             </Col>
 
-            <Col lg='3'></Col>
+            <Col lg='3'>
+              <div>
+                <h6 className='d-flex align-items-center justify-content-between'>
+                  Subtotal
+                  <span className='fs-4 fw-bold'>${totalAmount}</span>
+                </h6>
+              </div>
+              <p className='fs-6 mt-2'>
+                taxes and shipping will calculate in checkout
+              </p>
+              <div>
+                <button className='buy_btn w-100'>
+                  <Link to='/checkout'>Checkout</Link>
+                </button>
+                <button className='buy_btn w-100 mt-3'>
+                  <Link to='/shop'>Continue Shopping</Link>
+                </button>
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
     </Helmet>
+  );
+};
+
+const Tr = ({ item }) => {
+  const dispatch = useDispatch();
+
+  const deleteProduct = () => {
+    dispatch(cartActions.deleteItem(item.id));
+  };
+
+  return (
+    <tr>
+      <td>
+        <img src={item.imgUrl} alt='' />
+      </td>
+      <td>{item.productName}</td>
+      <td>${item.price}</td>
+      <td>{item.quantity}pcs</td>
+      <td>
+        <motion.i
+          whileTap={{ scale: 1.2 }}
+          onClick={deleteProduct}
+          class='ri-delete-bin-line'
+        ></motion.i>
+      </td>
+    </tr>
   );
 };
 
